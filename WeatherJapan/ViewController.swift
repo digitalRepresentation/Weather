cb1e7a96a3e5bc6b7ddedeeb701af3659ed5810f
digitalRepresentation <kinemura@naver.com>
2020-10-11 01:00:19 +0900

    Initial Commit

---
+++ WeatherJapan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherJapan
+//
+//  Created by 金宝ヨン on 2020/10/11.
+//  Copyright © 2020 kimjinyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
