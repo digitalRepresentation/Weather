af02e95c0d688c2fefe6391b200873b0dad9abda
digitalRepresentation <kinemura@naver.com>
2020-10-19 00:07:06 +0900

    天気APIKey追加　10/19

---
+++ WeatherJapan/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  WeatherJapan
+//
+//  Created by 金宝ヨン on 2020/10/18.
+//  Copyright © 2020 kimjinyoung. All rights reserved.
+//
+
+import Foundation
+
+let appKey = "f93762fb49b4075bd2844d989ef0f56";
