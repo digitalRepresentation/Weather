fdf1f04562c63601f50758e9dc8eb5797af079cf
digitalRepresentation <kinemura@naver.com>
2020-10-27 17:39:37 +0900

    Cell Outlet 追加(現在時間、３時間単位)

---
+++ WeatherJapan/Cell/ForecastTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  ForecastTableViewCell.swift
+//  WeatherJapan
+//
+//  Created by 金宝ヨン on 2020/10/27.
+//  Copyright © 2020 kimjinyoung. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    static let identifier = "ForecastTableViewCell"
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherJapan/Cell/SummaryTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  SummaryTableViewCell.swift
+//  WeatherJapan
+//
+//  Created by 金宝ヨン on 2020/10/27.
+//  Copyright © 2020 kimjinyoung. All rights reserved.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+
+    static let indentifier = "SummaryTableViewCell"
+    
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var minMaxLabel: UILabel!
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
